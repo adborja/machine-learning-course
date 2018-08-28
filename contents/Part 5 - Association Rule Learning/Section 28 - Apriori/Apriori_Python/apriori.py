56a657e067e7cd9c42326c3457678a0278d3556d
aborja <alvarodavid.borja@aurea.com>
2018-08-28 08:21:01 -0500

    Adding contents for deep learning

---
+++ contents/Part 5 - Association Rule Learning/Section 28 - Apriori/Apriori_Python/apriori.py
@@ -0,0 +1,15 @@
+# Apriori
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Data Preprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
+
+# Training Apriori on the dataset
+from apyori import apriori
